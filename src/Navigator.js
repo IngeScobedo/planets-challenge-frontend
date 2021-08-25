dabe64dc0300a253a10909e8d2aa40dbca13c692
Alan Dev <ingescobedoalan4@gmail.com>
2021-08-24 23:25:06 -0500

    10% Navbar

---
+++ src/Navigator.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Navigator.css'; 
+
+const Navigator = () => {
+  return (
+    <div className="nav">
+      <h1 className="nav__logo">PLANETS</h1>
+      <ul className="nav__list">
+        <li className="nav__list-element" >PLANET</li>
+        <li className="nav__list-element" >PLANET</li>
+        <li className="nav__list-element" >PLANET</li>
+        <li className="nav__list-element" >PLANET</li>
+        <li className="nav__list-element" >PLANET</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navigator;
